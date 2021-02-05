773c10b274a436030b4ee24483880335c73d19a3
xiaofei <alibaba22001@163.com>
2021-02-05 17:26:52 +0800

    update

--- example.cpp
+++ example.cpp
@@ -6,13 +6,18 @@
  *   文件名称：example.cpp
  *   创 建 者：肖飞
  *   创建日期：2021年02月05日 星期五 15时20分59秒
- *   修改日期：2021年02月05日 星期五 16时48分28秒
+ *   修改日期：2021年02月05日 星期五 17时25分17秒
  *   描    述：
  *
  *================================================================*/
 #include <pybind11/pybind11.h>
 #include <pybind11/numpy.h>
 #include <pybind11/stl.h>
+
+#include <iostream>
+#include <memory>
+
+
 #include "util_log.h"
 
 namespace py = pybind11;
@@ -49,6 +54,70 @@ std::vector<std::string> get_str(void)
 	return v;
 }
 
+template< typename T >
+class MyHolder
+{
+public:
+	explicit MyHolder( T *ptr = nullptr ) : ptr_( ptr ) {}
+
+	template< typename U > MyHolder( MyHolder< U > ptr ) :
+		ptr_( dynamic_cast< T * >( ptr.get() ) ) {}
+
+	MyHolder( const MyHolder &other, T *ptr ) : ptr_(ptr) {}
+
+	T *get() const
+	{
+		return ptr_;
+	}
+
+	T *operator-> () const
+	{
+		return ptr_;
+	}
+
+private:
+	T *ptr_;
+};
+
+class A
+{
+public:
+	virtual ~A() = default;
+	virtual void foo() const = 0;
+};
+
+class B
+{
+public:
+	virtual ~B() = default;
+	virtual void bar() const = 0;
+};
+
+class C : public A, public B
+{
+public:
+	C() = default;
+	virtual ~C() = default;
+	virtual void foo() const override
+	{
+		std::cout << "foo" << std::endl;
+	}
+	virtual void bar() const override
+	{
+		std::cout << "bar" << std::endl;
+	}
+};
+
+void testA(MyHolder<A> ptr)
+{
+	ptr->foo();
+}
+
+void testB(MyHolder<B> ptr)
+{
+	ptr->bar();
+}
+
 PYBIND11_MODULE(example, m)
 {
 	m.doc() = "pybind11 example plugin"; // optional module docstring
@@ -57,4 +126,10 @@ PYBIND11_MODULE(example, m)
 	m.def("set_data", &set_data, "Get NumPy arrays");
 	m.def("set_str", &set_str, "Set str");
 	m.def("get_str", &get_str, "Get str");
+	m.def("testa", &testA, "testA");
+	m.def("testb", &testB, "testB");
+	py::class_<MyHolder<char *>>(m, "MyHolder", py::dynamic_attr()).def(py::init<>());
+	py::class_<A>(m, "A", py::dynamic_attr());
+	py::class_<B>(m, "B", py::dynamic_attr());
+	py::class_<C>(m, "C", py::dynamic_attr()).def(py::init<>());
 }
